caf9a8cbee7d4366fde0808c19c10d558de12f2a
SpyrosGeo <xarodasimba@gmail.com>
2020-04-25 00:47:03 +0300

    added background and sprites also added music

--- app.js
+++ app.js
@@ -1,67 +1,81 @@
-const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body, Events,MouseConstraint,Mouse } = Matter;
 
 const engine = Engine.create();
-const {world } = engine;
+const { world } = engine;
 
-const width=window.innerWidth
-const height= window.innerHeight
-
-
+const width = window.innerWidth
+const height = window.innerHeight
+//audio
+const music = new Audio('/audio/music.mp3')
+const ambience = new Audio('/audio/ambience.mp3')
 const render = Render.create({
-    element:document.body,
-    engine:engine,
-    options:{
+    element: document.body,
+    engine: engine,
+    options: {
         width,
         height,
-        wireframes:true,
+        wireframes: false,
+        background:'rgba(70,50,22,0.4)' 
     }
 })
 
 Render.run(render);
-Runner.run(Runner.create(),engine);
+Runner.run(Runner.create(), engine);
+World.add(world,MouseConstraint.create(engine,{
+    mouse:Mouse.create(render.canvas)
+}))
 
 
-const walls = [
-   Bodies.rectangle(width / 2, height, width, 22, {
+//walls
+const walls = Bodies.rectangle(width / 2, height, width, 22, {
         isStatic: true,
-        label:"pavement"
-    }),
- 
-]
+        label: "pavement"
+    })
+
+World.add(world, walls)
 
 
-World.add(world,walls)
 
-
+//raindrops
 let raindrop;
-const generateRainDrops=()=>{
+const generateRainDrops = () => {
     randPositionX = Math.floor(Math.random() * width)
     raindrop = Bodies.circle(randPositionX, 0, 4, {
         isStatic: false,
         label: "raindrop",
-        render:{
-            sprite:{
-                texture:'./imgs/rain.png'
+        render: {
+            sprite: {
+                texture: './imgs/rain.png',
+                xScale:1,
+                yScale:1
             }
         }
     })
-    World.add(world,raindrop)
+    World.add(world, raindrop)
 
 }
 
-// generateRainDrops()
+// player
+const umbrella = Bodies.rectangle(width/2,height/2,30,30,{
+    isStatic:false
+})
 
-setInterval(()=>{
+// World.add(world,umbrella)
+//generate and check for collision 
+setInterval(() => {
     generateRainDrops()
-     Events.on(engine, 'collisionStart', e => {
-        e.forEach(collision=>{
-            World.remove(world,raindrop)
+    Events.on(engine, 'collisionStart', e => {
+        e.forEach(collision => {
+            World.remove(world, raindrop)
         })
     })
-},300)
+}, 100)
+
+window.addEventListener('load',()=>{
+    music.play()
+    ambience.play()
+})
 
 
-    // Events.on(engine, 'collisionStart', () => {
-    //     World.remove(world, raindrop)
-    // })
+   
 
